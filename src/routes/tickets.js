000670eed3734cecd9ff03294513761ebdb1d98e
Otavio <otaviocds03@gmail.com>
2025-09-29 20:39:15 -0300

    feat: handle the route of the request

---
+++ src/routes/tickets.js
@@ -0,0 +1,15 @@
+import { create } from "../controllers/tickets/create.js"
+import { list } from "../controllers/tickets/list.js"
+
+export const tickets = [
+    {
+        method: "GET",
+        path: "/tickets",
+        controller: list
+    },
+    {
+        method: "POST",
+        path: "/tickets",
+        controller: create
+    },
+]
